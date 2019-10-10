2e78f3c45b14369c73e6ed0a6168ddbdcc848169
Вадим Павлов <vadimpavlov@Mac-mini-Vadim.local>
2019-10-11 03:45:54 +0700

    сверстан блок brif, сделана валидация форм

---
+++ src/js/validate.js
@@ -0,0 +1,86 @@
+/* Форма блока offer*/
+$('#offer-form').validate({
+    rules: {
+        username: {
+            required: true,
+            minlength: 3,
+            maxlength: 15
+        },
+        phone: {
+            required: true
+        }
+    },
+    messages: {
+        username: {
+            required: "Укажите Ваше имя",
+            minlength: "Требуется не менее 3 символов!",
+            maxlength: "Требуется не более 15 символов!"
+        },
+        phone: {
+            required: "Укажите Ваш телефон"
+        }
+    }
+});
+
+/* Форма modal */
+$('#modal-form').validate({
+    rules: {
+        username: {
+            required: true,
+            minlength: 3,
+            maxlength: 15
+        },
+        phone: {
+            required: true
+        }
+    },
+    messages: {
+        username: {
+            required: "Укажите Ваше имя",
+            minlength: "Требуется не менее 3 символов!",
+            maxlength: "Требуется не более 15 символов!"
+        },
+        phone: {
+            required: "Укажите Ваш телефон"
+        }
+    }
+});
+
+/* Форма блока бриф */
+$('#brif-form').validate({
+    rules: {
+        username: {
+            required: true,
+            minlength: 3,
+            maxlength: 15
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        phone: {
+            required: true
+        }  
+    },
+    messages: {
+        username: {
+            required: "Укажите Ваше имя",
+            minlength: "Требуется не менее 3 символов!",
+            maxlength: "Требуется не более 15 символов!"
+        },
+        email: {
+            required: "Заполните поле",
+            email: "Введите корректный email"
+        },
+        phone: {
+            required: "Укажите Ваш телефон"
+        }
+    }
+});
+
+/* Маска для телефона в блоке offer */
+$("#offer-phone").mask('+7 (999) 999-99-99');
+/* Маска для телефона в форме modal */
+$("#modal-phone").mask('+7 (999) 999-99-99');
+/* Маска для телефона в блоке brif */
+$("#brif-phone").mask('+7 (999) 999-99-99');
