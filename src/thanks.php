590db7a4830b528f5e57e3ba43f3f70355dba68f
Вадим Павлов <vadimpavlov@Mac-mini-Vadim.local>
2019-10-15 00:21:36 +0700

    исправил ошибки

---
+++ src/thanks.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/logo.png" >
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Ремонт квартир</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-block">
+                <div class="navbar-block__logo wow flip" data-wow-delay="0.1s">
+                    <a href="index.html">
+                        <img src="img/logo.png" alt="">
+                    </a>
+                </div>
+                <!-- /.navbar-block__logo -->
+                <div class="navbar-block__info">
+                    <div class="navbar-block__contacts">
+                        <span class="navbar-block__address">Калуга, Москва, МО</span>
+                        <a href="tel:+7(495)42-251-31" class="navbar-block__phone">+7 (495) 42-251-31</a>
+                    </div>
+                    <!-- /.navbar__contacts -->
+                    <div class="btn">
+                        <a href="#" class="button navbar-block__button" id="button">Перезвоните мне</a>
+                    </div>
+                    <!-- /.button -->
+                   <!--  <button class="button navbar__button">Перезвоните мне</button> -->
+                </div>
+                <!-- /.navbar_info -->
+            </div>
+            <!-- /.navbar-block -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- /.navbar -->
+    <main>
+        <section class="hero">
+            <div class="container">
+                <div class="hero-block">
+                    <div class="hero-text">
+                        <h1 class="hero-text__title">
+                            Спасибо за заявку 
+                        </h1>
+                        <span class="hero-text__subtitle">
+                            Мы перезвоним через 15 минут
+                        </span>
+                        <ul class="hero-list">
+                            <li class="hero-list__item wow fadeInLeft" data-wow-delay="0.3s">
+                                <div class="hero-list__image">
+                                    <img src="img/hero/clock.png" alt="clock">
+                                </div>
+                                <span class="hero-list__text">Точно соблюдаем сроки</span>
+                            </li>
+                            <li class="hero-list__item wow fadeInLeft" data-wow-delay="0.5s">
+                                <div class="hero-list__image">
+                                    <img src="img/hero/calculator.png" alt="calculator">
+                                </div>
+                                <span class="hero-list__text">Рассчитаем смету на работы и материалы в день обращения</span>
+                            </li>
+                            <li class="hero-list__item wow fadeInLeft" data-wow-delay="0.7s">
+                                <div class="hero-list__image">
+                                    <img src="img/hero/paint-board.png" alt="paint-board">
+                                </div>
+                                <span class="hero-list__text">Предложим более 100 вариантов исполнения дизайна Вашего жилья</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /.hero-text -->
+                    <div class="hero-image">
+                        <img src="img/hero/hero-image.png" alt="План квартиры">
+                    </div>
+                    <!-- /.hero-image -->
+                </div>
+                <!-- /.hero-block -->
+            </div>
+            <!-- /.container -->
+        </section>
+        <!-- /.hero -->
+    </main>
+
+
+    <!-- <script src="js/main.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="js/modal.js"></script>
+    <script src="js/button-back.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/slider-slick.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/jquery.maskedinput.min.js"></script>
+    <script src="js/validate.js"></script>
+    
+    <script>
+        /* Инициализация WOW.js */
+        new WOW().init();
+    </script>
+</body>
+</html>
