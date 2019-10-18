1f82b5be87458f62e79a1997fc4a7e7a0e74b41c
Вадим Павлов <vadimpavlov@Mac-mini-Vadim.local>
2019-10-18 15:40:27 +0700

    настроил оптимизацию сайта и переделал карту

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const tinify = require('gulp-tinify');
+const jsmin = require('gulp-jsmin');
+
+/* Перенеси CSS файлы */
+gulp.task('move-css', () => {
+    return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('room/css/'))
+});
+/* Перени файлы MIN JS */
+gulp.task('move-js', () => {
+    return gulp.src('src/js/*.min.js')
+    .pipe(gulp.dest('room/js/'))
+});
+/* Перени файлы JS */
+gulp.task('minify-js', () => {
+    return gulp.src(['src/js/*.js', '!src/js/*.min.js'])
+    .pipe(jsmin())
+    .pipe(gulp.dest('room/js/'))
+});
+
+/* Сжать файлы JS */
+
+/* Перенести файлы IMG */
+gulp.task('imagemin', () => {
+    return gulp.src('src/img/**/*.*')
+    .pipe(tinify('bqvNq6gqG4JWbsKqLSHRt4l2JZ5xhsH1'))
+    .pipe(gulp.dest('room/img/'))
+});
+/* Перенести файлы FONTS*/
+gulp.task('move-fonts', () => {
+    return gulp.src('src/fonts/*/*.*')
+    .pipe(gulp.dest('room/fonts/'))
+});
+/* Перенести файлы HTML */
+gulp.task('move-html', () => {
+    return gulp.src('src/*.html')
+    .pipe(gulp.dest('room/'))
+});
---
+++ room/js/map.js
@@ -0,0 +1,9 @@
+
+var spinner=$('.ymap-container').children('.loader');var check_if_load=false;var myMapTemp,myPlacemarkTemp;function init(){var myMapTemp=new ymaps.Map("map-yandex",{center:[55.611413,37.2011344],zoom:18,controls:['zoomControl','fullscreenControl']});var myPlacemarkTemp=new ymaps.GeoObject({geometry:{type:"Point",coordinates:[55.611413,37.2011344]},properties:{balloonContent:"Ремонт квартир"}});myMapTemp.geoObjects.add(myPlacemarkTemp);var layer=myMapTemp.layers.get(0).get(0);waitForTilesLoad(layer).then(function(){spinner.removeClass('is-active');});}
+function waitForTilesLoad(layer){return new ymaps.vow.Promise(function(resolve,reject){var tc=getTileContainer(layer),readyAll=true;tc.tiles.each(function(tile,number){if(!tile.isReady()){readyAll=false;}});if(readyAll){resolve();}else{tc.events.once("ready",function(){resolve();});}});}
+function getTileContainer(layer){for(var k in layer){if(layer.hasOwnProperty(k)){if(layer[k]instanceof ymaps.layer.tileContainer.CanvasContainer||layer[k]instanceof ymaps.layer.tileContainer.DomContainer){return layer[k];}}}
+return null;}
+function loadScript(url,callback){var script=document.createElement("script");if(script.readyState){script.onreadystatechange=function(){if(script.readyState=="loaded"||script.readyState=="complete"){script.onreadystatechange=null;callback();}};}else{script.onload=function(){callback();};}
+script.src=url;document.getElementsByTagName("head")[0].appendChild(script);}
+var ymap=function(){$('.ymap-container').mouseenter(function(){if(!check_if_load){check_if_load=true;spinner.addClass('is-active');loadScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;loadByRequire=1",function(){ymaps.load(init);});}});}
+$(function(){ymap();});
